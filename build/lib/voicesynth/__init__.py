ac74c62fe774a27c98b9a59db76690299cd55d0c
ilyamiro <ilyamiro.work@gmail.com>
2026-03-13 23:07:21 +0100

    Clean-up github repository from build directories. Fix of the previous commit

--- build/lib/voicesynth/__init__.py
+++
@@ -1,9 +0,0 @@
-from voicesynth.voicesynth import (
-    Synthesizer, Model, show_available_models, disable_logging
-)
-
-
-__all__ = ["Synthesizer", "Model", "show_available_models", "disable_logging"]
-
-
-
